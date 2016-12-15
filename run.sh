2c38d1f42c221d53bb331fe618bdda7ff7296f57
cambierr <romain@shareif.com>
2016-12-15 19:18:35 +0100

    fixed travis build script (again)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH = "master" ]]; then
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out pubring.gpg -d
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out secring.gpg -d
     mvn clean validate compile test package verify install deploy --settings settings.xml
