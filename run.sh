d90568f143ae48b51bb1107a3566327e01c2c05b
cambierr <romain@shareif.com>
2016-12-16 12:28:17 +0100

    updated config to automate sonatype uploads + javadoc generation and publication

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out pubring.gpg -d
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out secring.gpg -d
-    mvn clean validate compile test package verify install deploy --settings settings.xml
+    mvn clean package install javadoc:aggregate javadoc:jar source:jar-no-fork deploy --settings settings.xml -DperformRelease=true
     exit $?
 else
-    mvn clean validate compile test package verify
+    mvn clean package javadoc:aggregate javadoc:jar source:jar-no-fork
     exit $?
 fi
