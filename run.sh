7785ff8f13626db206b16e774ee3b72f99637b8c
cambierr <romain@shareif.com>
2016-12-16 15:40:58 +0100

    configured maven to avoid deploying samples

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH = "fix-ci" ]]; then
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
     openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
+    rm -rf samples
     mvn clean package install javadoc:aggregate javadoc:jar source:jar-no-fork deploy --settings settings.xml -DperformRelease=true
     #publish doc
     rm -rf out
