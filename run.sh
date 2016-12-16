1e6c9816c4306d897bd235445a96fa6dd3269c51
Romain Cambier <cambierr@users.noreply.github.com>
2016-12-16 16:17:33 +0100

    Fix ci (#36)
    * test
    
    * fixed keypath that was relative instead of absolute
    
    * fixed malformed settings file
    
    * configured maven to avoid deploying samples
    
    * configured maven to avoid deploying samples
    
    * configured maven to avoid deploying samples
    
    * configured maven to avoid deploying samples
    
    * removed samples from module list
    
    * removed samples from module list
    
    * ready for merge into master

--- run.sh
+++ run.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out pubring.gpg -d
-    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out secring.gpg -d
-    mvn clean package install javadoc:aggregate javadoc:jar source:jar-no-fork deploy --settings settings.xml -DperformRelease=true
-    #publish doc
-    rm -rf out
-    mkdir out
-    cd out
-    git init
-    git config user.name "Travis-CI"
-    git config user.email "travis@java-app-sdk"
-    cp -r ../target/site/apidocs/* ./
-    git add .
-    git commit -m "Deployed to Github Pages"
-    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d \
+    && openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d \
+    && cd samples \
+    && mvn clean package \
+    && cd ../ \
+    && mvn clean package install javadoc:aggregate javadoc:jar source:jar-no-fork deploy --settings settings.xml -DperformRelease=true \
+    && rm -rf out \
+    && mkdir out \
+    && cd out \
+    && git init \
+    && git config user.name "Travis-CI" \
+    && git config user.email "travis@java-app-sdk" \
+    && cp -r ../target/site/apidocs/* ./ \
+    && git add . \
+    && git commit -m "Deployed to Github Pages" \
+    && git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
     exit $?
 else
-    mvn clean package javadoc:aggregate javadoc:jar source:jar-no-fork
+    mvn clean package javadoc:aggregate javadoc:jar source:jar-no-fork \
+    && cd samples \
+    && mvn clean package
     exit $?
 fi
