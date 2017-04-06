514ecaff0d18fdb97832ec5382d9bf924547ba42
cambierr <me@romaincambier.be>
2017-04-06 14:49:51 +0200

    implemented #51

---
+++ account/src/main/java/org/thethingsnetwork/account/async/auth/token/AsyncAccessKey.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thethingsnetwork.account.async.auth.token;
+
+import java.net.URI;
+import org.thethingsnetwork.account.common.GrantType;
+import rx.Observable;
+
+/**
+ *
+ * @author Romain Cambier
+ */
+public class AsyncAccessKey implements AsyncOAuth2Token {
+
+    private final String accessKey;
+    private final URI accountServer;
+
+    public AsyncAccessKey(String _accessKey) {
+        this(_accessKey, GrantType.DEFAULT_ACCOUNT_SERVER);
+    }
+
+    public AsyncAccessKey(String _accessKey, URI _accountServer) {
+        accessKey = _accessKey;
+        accountServer = _accountServer;
+    }
+
+    @Override
+    public boolean hasRefresh() {
+        return false;
+    }
+
+    @Override
+    public Observable<? extends AsyncOAuth2Token> refresh() {
+        return Observable.error(new UnsupportedOperationException("Not supported."));
+    }
+
+    @Override
+    public boolean isExpired() {
+        return false;
+    }
+
+    @Override
+    public String getToken() {
+        return "Key " + accessKey;
+    }
+
+    @Override
+    public String getRawToken() {
+        return accessKey;
+    }
+
+    @Override
+    public URI getAccountServer() {
+        return accountServer;
+    }
+
+}
